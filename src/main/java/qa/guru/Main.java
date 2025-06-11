a2d1bd92aeece769b5cc3449f745727a559cf39b
Roman Filatov <110454660+RomanFilatov1982@users.noreply.github.com>
2025-06-11 19:48:22 +0300

    Update Main.java

--- src/main/java/qa/guru/Main.java
+++ src/main/java/qa/guru/Main.java
@@ -1,7 +1,8 @@
 package qa.guru;
 
 public class Main {
+    private static String msg ="Hello qa_guru!!!!!!";
     public static void main(String[] args) {
-        System.out.println("Hello qa_guru!!!!!!");
+        System.out.println("msg");
     }
 }
